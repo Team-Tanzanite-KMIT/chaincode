725e99bd2af0491c8123d75fc3faff6973397328
brohithkr <brohithkr@gmail.com>
2023-11-14 19:02:20 +0530

    basic operations

--- src/assetTransfer.ts
+++ src/assetTransfer.ts
@@ -79,7 +79,23 @@ export class FileTransferContract extends Contract {
     @Transaction(false)
     @Returns('string')
     public async GetAllFiles(ctx: Context) {
-        
+        const allResults = [];
+        // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
+        const iterator = await ctx.stub.getStateByRange('', '');
+        let result = await iterator.next();
+        while (!result.done) {
+            const strValue = Buffer.from(result.value.value.toString()).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            allResults.push(record);
+            result = await iterator.next();
+        }
+        return JSON.stringify(allResults);
     }
 
 }
